a1920d99f4837cb21f5d22785a7774d8a67753dc
Unknown <nonlapan.1998@mail.kmutt.ac.th>
2019-04-13 23:25:55 +0700

    UI View Product

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom'
+import MaterialTable from 'material-table'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div style={{ maxWidth: '100%',height: '100%'}}>
+      <MaterialTable
+        columns={[
+          { title: 'Name', field: 'name' },
+          { title: 'Detail', field: 'detail' },
+          { title: 'Price', field: 'price', type: '' },
+          { title: 'Picture', field: 'pic' }
+        ]}
+        data={[{ name: '1', detail: 'Baran', price: 1987, pic: 63 }]}
+        title="Product"
+      />
+    </div>
+     
+    
     );
   }
 }
-
 export default App;
