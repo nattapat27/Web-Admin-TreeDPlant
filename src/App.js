ad334f2c717b66ca49cd0643a70592325fd56fed
Unknown <nonlapan.1998@mail.kmutt.ac.th>
2019-05-26 19:52:41 +0700

    Edit bug merce

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 //import ReactDOM from 'react-dom'
 //import MaterialTable from 'material-table'
 import axios from 'axios';
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 // import App from './Components/App';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-    ReactDOM.render(<ShowProduct />, document.getElementById('product'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
